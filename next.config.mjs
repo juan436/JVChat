c191ab6fb42bce2fe0721c439e77d446e4e2f0b0
juan <juancvillefer@gmail.com>
2025-07-07 22:34:30 -0400

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  skipTrailingSlashRedirect: true,
+  experimental: {
+    swcTraceProfiling: true,
+    forceSwcTransforms: true,
+  },
   onDemandEntries: {
     maxInactiveAge: 25 * 1000,
     pagesBufferLength: 2,
-  },
-  experimental: {
-    skipTrailingSlashRedirect: true,
-    swcTraceProfiling: true,
-    forceSwcTransforms: true,
   },
 };
 
